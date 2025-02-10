ff826c535a91f4187893ae6f4d3b7889f7056d20
blob5 <101657201+blob5@users.noreply.github.com>
2025-02-10 14:49:14 +0100

    refactor: update hardware configuration for graphics and audio services

--- hosts/cyberia/imports/amd-drivers.nix
+++ hosts/cyberia/imports/amd-drivers.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  hardware.opengl.enable = true; # Enable OpenGL support
+  hardware.graphics.enable = true; # Enable OpenGL support
   services.xserver.videoDrivers = [ "amdgpu" ]; # Use the AMDGPU driver
   hardware.enableRedistributableFirmware = true; # Ensure GPU firmware is available
 }
--- hosts/cyberia/imports/services.nix
+++ hosts/cyberia/imports/services.nix
@@ -9,7 +9,7 @@
 
   services.openssh.enable = true;
 
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
