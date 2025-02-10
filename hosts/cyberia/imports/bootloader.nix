2d1b39548a9cacd6e4b9ef6b97ef7b7bf9e6b4f7
blob5 <101657201+blob5@users.noreply.github.com>
2025-02-10 16:18:20 +0100

    woops

--- hosts/cyberia/imports/bootloader.nix
+++ hosts/cyberia/imports/bootloader.nix
@@ -1,26 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  boot {
-    kernelParams = [ "acpi=force" "acpi_osi=" ];
-    loader = {
-      grub = {
-        useOSProber = true;
+  boot.loader = {
+    grub = {
+      useOSProber = true;
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+
+      configurationLimit = 10;
+
+      minegrub-world-sel = {
         enable = true;
-        efiSupport = true;
-        device = "nodev";
-
-        configurationLimit = 10;
-
-        minegrub-world-sel = {
-          enable = true;
-          customIcons = [{
-            name = "nixos";
-            lineTop = "NixOS (23/10/2024, 23:03)";
-            lineBottom = "Survival Mode, No Cheats, Version: 24.05";
-            imgName = "nixos";
-          }];
-        };
+        customIcons = [{
+          name = "nixos";
+          lineTop = "NixOS (23/10/2024, 23:03)";
+          lineBottom = "Survival Mode, No Cheats, Version: 24.05";
+          imgName = "nixos";
+        }];
       };
     };
   };
