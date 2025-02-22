9b080fb4afce2950c5d9d141131ec9326f684a7b
blob <101657201+blob5@users.noreply.github.com>
2025-02-23 00:03:00 +0100

    moved wezterm config to separate file

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -5,25 +5,6 @@
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
-    extraConfig = ''
-
--- Pull in stuff
-local wezterm = require 'wezterm'
-local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
-
--- This will hold the configuration.
-local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
--- apply themes
-config.color_scheme = 'Catppuccin Frappé (Gogh)'
-bar.apply_to_config(config)
-
--- and finally, return the configuration to wezterm
-return config
-
-'';
-
+    extraConfig = builtins.readFile ./wezterm.lua;
   };
 }
--- home/programs/wezterm/wezterm.lua
+++ home/programs/wezterm/wezterm.lua
@@ -1,13 +1,16 @@
--- Pull in the wezterm API
+
+-- Pull in stuff
 local wezterm = require 'wezterm'
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- apply themes
 config.color_scheme = 'Catppuccin Frappé (Gogh)'
+bar.apply_to_config(config)
 
 -- and finally, return the configuration to wezterm
 return config
