e1e5a729391b692d3a3551bc4805b3060159de94
blob <101657201+blob5@users.noreply.github.com>
2025-03-01 15:36:30 +0100

    changed cursor theme

--- hosts/cyberia/home.nix
+++ hosts/cyberia/home.nix
@@ -78,6 +78,14 @@
   };
   
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic"; 
+    size = 24;
+  };
+
   programs.git = {
     enable = true;
     userName = "blob";
