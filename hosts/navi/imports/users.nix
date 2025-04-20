f06dd9022c09d1a91fdfa28efb9589d806d3fe02
blob5 <101657201+blob5@users.noreply.github.com>
2025-04-20 16:43:02 +0200

    Add wireshark group to blob user configuration

--- hosts/navi/imports/users.nix
+++ hosts/navi/imports/users.nix
@@ -4,7 +4,7 @@
   users.users.blob = {
     isNormalUser = true;
     description = "blob";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
+    extraGroups = [ "networkmanager" "wheel" "docker" "wireshark" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
       # Add any additional packages for the user here
