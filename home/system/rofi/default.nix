a37670582d3ae12e568867ff4772c797d1a1d494
blob5 <101657201+blob5@users.noreply.github.com>
2025-04-24 12:36:05 +0200

    Fix rofi theme path

--- home/system/rofi/default.nix
+++ home/system/rofi/default.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }:
 
 {
+  home.file.".config/rofi/spotlight-dark.rasi".source = ./spotlight-dark.rasi;
+
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
-    theme = builtins.toPath ./spotlight-dark.rasi;
+    theme = "~/.config/rofi/spotlight-dark.rasi";
   };
-   
-  
+
+
   # for rofi-emoji to insert emojis directly
   home.packages = [ pkgs.xdotool ];
 }
