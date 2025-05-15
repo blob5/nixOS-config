c78847a3436120bf3c04aeee946678ecec2e6273
blob5 <101657201+blob5@users.noreply.github.com>
2025-05-15 20:47:00 +0200

    Remove font configurations from stylix default.nix

--- home/programs/stylix/default.nix
+++ home/programs/stylix/default.nix
@@ -13,26 +13,4 @@
     qt.enable = true;
     mpv.enable = true;
   };
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-
-    monospace = {
-      package = pkgs.nerd-fonts.jetbrains-mono;
-      name = "JetBrains Mono";
-    };
-
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
-  };
 }
