4147ff7dfc4834271c82e8263ff68aca02c5b9b2
blob5 <101657201+blob5@users.noreply.github.com>
2025-05-18 20:06:31 +0200

    Add systemd service for automatic Tailscale connection

--- hosts/common/common-configuration.nix
+++ hosts/common/common-configuration.nix
@@ -4,4 +4,18 @@
 	nix.settings.auto-optimise-store = true;
 	nix.gc.automatic = true;
 	services.dbus.enable = true;
+
+	systemd.services.tailscale-autoconnect = {
+		description = "Auto-connect to Tailscale";
+		after = [ "network-online.target" "tailscaled.service" ];
+		wants = [ "network-online.target" ];
+		wantedBy = [ "multi-user.target" ];
+
+		serviceConfig = {
+			Type = "oneshot";
+			ExecStart = ''
+			${pkgs.tailscale}/bin/tailscale up
+			'';
+		};
+		};
 }
