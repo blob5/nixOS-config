ce41ca4d93a562e18289a8a99079ff1d4ff3a157
blob5 <101657201+blob5@users.noreply.github.com>
2025-05-23 23:42:18 +0200

    Removed spaces between inputs

--- flake.nix
+++ flake.nix
@@ -4,24 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
-  
     fastanime.url = "github:Benexl/fastanime";
     lobster.url = "github:justchokingaround/lobster";
-
     better-control.url = "github:rishabh5321/better-control-flake";
-
     spicetify-nix.url = "github:Gerg-L/spicetify-nix"; 
-
     stylix.url = "github:danth/stylix";  
-   
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
- 
     minegrub-world-sel-theme.url = "github:Lxtharia/minegrub-world-sel-theme";
-
     swww.url = "github:LGFae/swww";
-
     niri.url = "github:sodiboo/niri-flake";
-
     ags.url = "github:Aylur/ags";
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     hyprland.url = "github:hyprwm/Hyprland";  
