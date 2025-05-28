cc9bc4a847dbe7b1cf203c79f5352378c79da2f7
blob5 <101657201+blob5@users.noreply.github.com>
2025-05-28 20:46:09 +0200

    Enhance stylix configuration with additional font and icon theme settings

--- home/programs/stylix/default.nix
+++ home/programs/stylix/default.nix
@@ -7,10 +7,45 @@
 
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
+  stylix.polarity = "dark";
+
   stylix.targets = {
     gtk.enable = true;
     kde.enable = true;
-    qt.enable = true;
+    qt = {
+      enable = true;
+      platform = "qtct";
+    };
     mpv.enable = true;
   };
+
+  stylix.iconTheme ={
+    enable = true;
+    package = pkgs.papirus-icon-theme;
+    dark = "Papirus-Dark";
+    light = "Papirus-Light";
+  };
+
+  stylix.fonts = {
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
+
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+
+    monospace = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans Mono";
+    };
+
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
+
 }
