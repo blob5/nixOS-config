2d60a3d07627671c91c16afd7770a4c35e6286d6
blob5 <101657201+blob5@users.noreply.github.com>
2025-05-31 00:46:24 +0200

    Fix syntax error in sessionPackages assignment in sddm configuration

--- modules/services/sddm.nix
+++ modules/services/sddm.nix
@@ -6,7 +6,7 @@ in
 {
   # sddm
   services.displayManager = {
-    sessionPackages = with pkgs; [systemSettings.compositor]
+    sessionPackages = with pkgs; [systemSettings.compositor];
     sddm = {
       wayland.enable = true;
       enable = true;
