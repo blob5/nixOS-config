5ff849cefd7be06a1da49675d05379c72b6d08d8
blob5 <101657201+blob5@users.noreply.github.com>
2025-05-31 00:32:04 +0200

    Remove duplicate Bluetooth configuration from cyberia settings

--- hosts/cyberia/configuration.nix
+++ hosts/cyberia/configuration.nix
@@ -5,23 +5,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  
-    hardware.bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-      #package = pkgs.bluez5-experimental;
-      package = pkgs.bluez;
-      settings.Policy.AutoEnable = "true";
-      settings.General = {
-          Enable = "Source,Sink,Media,Socket";
-          Name = "Hello";
-          ControllerMode = "dual";
-          FastConnectable = "true";
-          Experimental = "true";
-          KernelExperimental = "true";
-      };
-    };
-  
+    
   # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8"; # AM and PM but dd/mm/yy
 
