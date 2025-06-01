07d4812105445c5ef165140a44c3a6e39449424b
blob5 <101657201+blob5@users.noreply.github.com>
2025-06-01 23:28:27 +0200

    Remove Wayland DISPLAY fix for Niri from Zsh configuration

--- modules/programs/terminals/zsh/zsh.nix
+++ modules/programs/terminals/zsh/zsh.nix
@@ -68,11 +68,6 @@
       bindkey "^[[1;5D" backward-word # ctrl+right arrow
       bindkey "^H" backward-kill-word # ctrl+backspace
 
-      # Fix for niri when running wayland
-      if pgrep -x niri >/dev/null; then
-        export DISPLAY=:0
-      fi
-
 
       # Enable color support
       export CLICOLOR=1
