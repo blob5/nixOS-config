7116115842ac9e2f113a57da301002f993d9a8f6
blob5 <101657201+blob5@users.noreply.github.com>
2025-06-24 23:27:09 +0200

    Add additional groups to blob user configuration

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -4,7 +4,7 @@
   users.users.blob = {
     isNormalUser = true;
     description = "blob";
-    extraGroups = [ "networkmanager" "wheel" "docker" "wireshark" "syncthing"];
+    extraGroups = [ "networkmanager" "wheel" "docker" "wireshark" "syncthing" "libvirt" "kvm" "input"];
     shell = pkgs.zsh;
     packages = with pkgs; [
       # Add any additional packages for the user here
