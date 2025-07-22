d000f834843fb101921ad37dff204d1fde5837e9
blob5 <101657201+blob5@users.noreply.github.com>
2025-07-22 14:15:39 +0200

    Update Tailscale autostart to start after sddm for fast boot times

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -10,19 +10,23 @@ in
   networking.firewall.enable = true;
   networking.nat.enable = true;
 
+  # Speeds up boot times by not waiting to be online
+  # Probably not smart to have this on for servers or machines with remote mounts.
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Auto-connect to Tailscale on boot
   systemd.services.tailscale-autoconnect = {
-		description = "Auto-connect to Tailscale";
-		after = [ "network-online.target" "tailscaled.service" ];
-		wants = [ "network-online.target" ];
-		wantedBy = [ "multi-user.target" ];
-
-		serviceConfig = {
-			Type = "oneshot";
-			ExecStart = ''
-			${pkgs.tailscale}/bin/tailscale up
-			'';
-		};
-	};
+    description = "Auto-connect to Tailscale after login";
+    after = [
+      "network-online.target"
+      "tailscaled.service"
+      "sddm.service"
+    ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.tailscale}/bin/tailscale up";
+    };
+  };
 }
