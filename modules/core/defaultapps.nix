721b0923af50afd9b2da5c0f63ca4aab50752df2
blob5 <101657201+blob5@users.noreply.github.com>
2025-08-24 17:22:17 +0200

    change gwenview to default app for opening images

--- modules/core/defaultapps.nix
+++ modules/core/defaultapps.nix
@@ -5,6 +5,15 @@
 
   xdg.mime.defaultApplications = {
     "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+
+    # Common image MIME types
+    "image/png" = [ "org.kde.gwenview.desktop" ];
+    "image/jpeg" = [ "org.kde.gwenview.desktop" ];
+    "image/jpg" = [ "org.kde.gwenview.desktop" ];
+    "image/webp" = [ "org.kde.gwenview.desktop" ];
+    "image/gif" = [ "org.kde.gwenview.desktop" ];
+    "image/bmp" = [ "org.kde.gwenview.desktop" ];
+    "image/tiff" = [ "org.kde.gwenview.desktop" ];
   };
 
 }
