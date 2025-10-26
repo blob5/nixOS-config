1f17f6a309331afd68e0d239720ed891535b8053
blob5 <101657201+blob5@users.noreply.github.com>
2025-10-26 23:11:19 +0100

    update git settings to new definitions

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -7,8 +7,12 @@ in
   # Host-specific git settings
   programs.git = {
     enable = true;
-    userName = "blob5";
-    userEmail = "101657201+blob5@users.noreply.github.com";
+    settings = {
+      user = {
+        email = "101657201+blob5@users.noreply.github.com";
+        name = "blob5";
+      };
+    };
     signing = {
       key = "${hostSettings.gpgkey}";
       signByDefault = true;
