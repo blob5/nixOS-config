c7467ee33f7d4fe35652e7326bf75e0c0a1ad3d5
blob5 <101657201+blob5@users.noreply.github.com>
2025-11-27 11:40:19 +0100

    update gpg key on cache

--- hosts/cache/settings.nix
+++ hosts/cache/settings.nix
@@ -7,7 +7,7 @@
   hasNvidia = false;
   fileSystemCreationDate = "21 November 21:44:02 2024";
   compositor = "hyprland";
-  gpgkey = "278803B57DAB54E6";
+  gpgkey = "56C537693D9CD600";
 
   # This is mostly used to reference the location of the source files outside of the Nix store
   # For example, the waybar-dev alias in zsh.nix configuration files
