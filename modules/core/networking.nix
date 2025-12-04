0d2aac35439c99c11b71c4e9237c1cb2401d19c2
blob5 <101657201+blob5@users.noreply.github.com>
2025-12-04 20:18:18 +0100

    Enabled nftables and set checkreversepath to loose

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -9,7 +9,9 @@ in
   networking.wireguard.enable = true;
   networking.firewall.enable = true;
   networking.nat.enable = true;
+  networking.nftables.enable = true;
 
+  networking.firewall.checkReversePath = "loose"; # This is needed for libvirt guest-to-guest communication.
   # Speeds up boot times by not waiting to be online
   # Probably not smart to have this on for servers or machines with remote mounts.
   systemd.services.NetworkManager-wait-online.enable = false;
