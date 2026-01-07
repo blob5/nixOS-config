0a29b8d28af839216e527dd253d368a56ea8ae8f
blob5 <101657201+blob5@users.noreply.github.com>
2026-01-07 11:40:52 +0100

    add extra binary caches and disable zramswap

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -16,11 +16,15 @@ in
         "https://cache.flox.dev"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://attic.xuyh0120.win/lantian" # nix-cachyos-kernel
+        "https://cache.garnix.io"
       ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
@@ -29,7 +33,7 @@ in
   services.earlyoom.enableNotifications = true; # Possible DoS vector if untrusted users on same pc.
 
   zramSwap = {
-    enable = true;
+    enable = false;
     algorithm = "lz4";
   };
 
