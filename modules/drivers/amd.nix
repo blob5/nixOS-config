a513212332b3fba5822f0aed0f22507e55239130
blob5 <101657201+blob5@users.noreply.github.com>
2026-01-17 01:43:09 +0100

    update amd driver config

--- modules/drivers/amd.nix
+++ modules/drivers/amd.nix
@@ -1,7 +1,15 @@
 { ... }:
 
 {
-  hardware.graphics.enable = true; # Enable OpenGL support
-  services.xserver.videoDrivers = [ "amdgpu" ]; # Use the AMDGPU driver
-  hardware.enableRedistributableFirmware = true; # Ensure GPU firmware is available
+  hardware.graphics.enable = true;
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  boot.kernelParams = [
+    "amdgpu.dc=1"
+  ];
 }
