6693a9d651e1812abd6e1d586649efaff0f20a4a
blob5 <101657201+blob5@users.noreply.github.com>
2026-02-02 15:16:03 +0100

    Refactor OpenRGB service dependencies and environment variables

--- modules/services/home/openrgb.nix
+++ modules/services/home/openrgb.nix
@@ -5,16 +5,22 @@
 
   systemd.user.services.openrgb = {
     description = "OpenRGB with profile";
-    after = [ "graphical-session.target" "network.target" ];
+    after = [ "graphical-session.target" ];
     wants = [ "graphical-session.target" ];
 
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --profile gaming --startminimized";
+      
+      Environment = [
+        "DISPLAY=:0"
+        "WAYLAND_DISPLAY=wayland-0"
+      ];
+
       Restart = "on-failure";
       RestartSec = 5;
     };
 
-    wantedBy = [ "graphical-session.target" ];
+    wantedBy = [ "default.target" ];
   };
 }
