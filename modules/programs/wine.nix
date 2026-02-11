7617f3179af2264e887168646802b8edb79a516f
blob5 <101657201+blob5@users.noreply.github.com>
2026-02-11 14:17:48 +0100

    remove wineasio

--- modules/programs/wine.nix
+++ modules/programs/wine.nix
@@ -4,7 +4,6 @@
   environment.systemPackages = with pkgs; [
       wineWow64Packages.stagingFull
       winetricks
-      wineasio
       bottles # easy wine prefix management
       wineWow64Packages.yabridge
     ];
