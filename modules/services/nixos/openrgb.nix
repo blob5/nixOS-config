2541de998f48b1b9bb62159d4a3f11327f430a29
blob5 <101657201+blob5@users.noreply.github.com>
2026-02-11 14:10:31 +0100

    update openrgb service to fetch username dynamically

--- modules/services/nixos/openrgb.nix
+++ modules/services/nixos/openrgb.nix
@@ -1,13 +1,15 @@
 # modules/services/openrgb.nix
-{ pkgs, ... }:
-
+{ pkgs, systemSettings, inputs, ... }:
+let
+  hostSettings = import "${inputs.self}/hosts/${systemSettings.hostname}/settings.nix";
+in
 {
   # Required for i2c access by OpenRGB
   boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
   services.udev.packages = [ pkgs.openrgb-with-all-plugins ];
 
   # Home manager user service
-  home-manager.users.blob = {
+  home-manager.users.${hostSettings.username} = {
     systemd.user.services.openrgb = {
       Unit = {
         Description = "OpenRGB with profile";
