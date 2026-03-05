2bdd552a86f18556f2dc1364f318849268331729
blob5 <101657201+blob5@users.noreply.github.com>
2026-03-05 14:41:26 +0100

    enhance mpv configuration with audio backend support in VapourSynth setup

--- modules/packages/home/entertainment.nix
+++ modules/packages/home/entertainment.nix
@@ -1,41 +1,44 @@
 { inputs, pkgs, ... }:
 
 let
-  # First create mpv-unwrapped with vapoursynth support
-  mpv-unwrapped-with-vapoursynth = pkgs.mpv-unwrapped.override {
-    vapoursynthSupport = true;
-  };
-  
-  # Then build mpv with that unwrapped version and scripts
-  mpv-with-vapoursynth = pkgs.mpv.override {
-    mpv-unwrapped = mpv-unwrapped-with-vapoursynth;
-    scripts = with pkgs.mpvScripts; [
-      uosc       # Modern UI
-      mpris      # Media player remote interface
-    ];
-  };
+# First create mpv-unwrapped with vapoursynth AND audio support
+mpv-unwrapped-with-vapoursynth = pkgs.mpv-unwrapped.override {
+  vapoursynthSupport = true;
+  # Add audio backend support
+  alsaSupport = true;
+  pulseSupport = true;
+  pipewireSupport = true;
+};
 
-  # Wrap viu to use our VapourSynth-enabled mpv
-  viu-wrapped = pkgs.symlinkJoin {
-    name = "viu-with-vapoursynth-mpv";
-    paths = [ inputs.viu.packages."${pkgs.system}".default ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      # Remove the original viu wrapper to avoid double wrapping
-      rm -f $out/bin/viu
-      # Copy the actual viu Python script
-      if [ -e ${inputs.viu.packages."${pkgs.system}".default}/bin/.viu-wrapped ]; then
-        cp ${inputs.viu.packages."${pkgs.system}".default}/bin/.viu-wrapped $out/bin/viu
-      else
-        cp ${inputs.viu.packages."${pkgs.system}".default}/bin/viu $out/bin/viu
-      fi
-      chmod +x $out/bin/viu
-      
-      # Wrap viu to prepend our VapourSynth mpv to PATH
-      wrapProgram $out/bin/viu \
-        --prefix PATH : "${mpv-with-vapoursynth}/bin"
-    '';
-  };
+# Then build mpv with that unwrapped version and scripts
+mpv-with-vapoursynth = pkgs.mpv.override {
+  mpv-unwrapped = mpv-unwrapped-with-vapoursynth;
+  scripts = with pkgs.mpvScripts; [
+    uosc       # Modern UI
+    mpris      # Media player remote interface
+  ];
+};
+
+# Wrap viu to use our VapourSynth-enabled mpv
+viu-wrapped = pkgs.symlinkJoin {
+  name = "viu-with-vapoursynth-mpv";
+  paths = [ inputs.viu.packages."${pkgs.system}".default ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    # Remove the original viu wrapper to avoid double wrapping
+    rm -f $out/bin/viu
+    # Copy the actual viu Python script
+    if [ -e ${inputs.viu.packages."${pkgs.system}".default}/bin/.viu-wrapped ]; then
+      cp ${inputs.viu.packages."${pkgs.system}".default}/bin/.viu-wrapped $out/bin/viu
+    else
+      cp ${inputs.viu.packages."${pkgs.system}".default}/bin/viu $out/bin/viu
+    fi
+    chmod +x $out/bin/viu
+    # Wrap viu to prepend our VapourSynth mpv to PATH
+    wrapProgram $out/bin/viu \
+      --prefix PATH : "${mpv-with-vapoursynth}/bin"
+  '';
+};
 
   # Wrap lobster to use our VapourSynth-enabled mpv
   lobster-wrapped = pkgs.symlinkJoin {
