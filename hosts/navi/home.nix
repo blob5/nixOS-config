a54b871558563afe8c441ea24bc4587dcafd5979
blob5 <101657201+blob5@users.noreply.github.com>
2026-03-17 17:35:58 +0100

    update navi monitor settings

--- hosts/navi/home.nix
+++ hosts/navi/home.nix
@@ -40,7 +40,7 @@
   ];
 
   hyprland = {
-    monitors = [ "DP-1,2560x1440@240,0x0,1" "DP-3,2560x1440@75,2560x-600,1,transform,3" ];
+    monitors = [ "DP-1,2560x1440@240,0x0,1" "DP-3,2560x1440@120,2560x-600,1,transform,3" ];
     input.sensitivity = -0.75;
     input.kb_layout = "us";
     accel_profile = "flat";
