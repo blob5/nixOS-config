f212897180c0b56067e134fc332c40829a552f6d
blob5 <101657201+blob5@users.noreply.github.com>
2026-03-18 08:49:57 +0100

    replaced wezterm with kitty on cyberia

--- hosts/cyberia/home.nix
+++ hosts/cyberia/home.nix
@@ -7,7 +7,7 @@
 
     # Programs 
     ../../modules/programs/editors/vscode.nix
-    ../../modules/programs/terminals/wezterm/wezterm.nix
+    ../../modules/programs/terminals/kitty/kitty.nix
     ../../modules/programs/terminals/zsh/zsh.nix
     ../../modules/programs/spicetify/spicetify.nix
     ../../modules/programs/nixcord.nix
