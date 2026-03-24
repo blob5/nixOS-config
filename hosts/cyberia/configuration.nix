272a4e0e193dfdcbbe477d5b5646e5ea2349f2d6
blob5 <101657201+blob5@users.noreply.github.com>
2026-03-25 00:56:56 +0100

    Add UWSM configuration for Niri compositor

--- hosts/cyberia/configuration.nix
+++ hosts/cyberia/configuration.nix
@@ -3,6 +3,15 @@
 {
   programs.niri.enable = true;
 
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors.niri = {
+      prettyName = "Niri";
+      comment = "Niri compositor managed by UWSM";
+      binPath = "/run/current-system/sw/bin/niri";
+    };
+  };
+
   imports =
     [
       ## Hardware configuration
