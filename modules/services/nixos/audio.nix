10c421ceb4f496125a3f21551fc8486a0e06a9c1
blob5 <101657201+blob5@users.noreply.github.com>
2026-05-04 22:21:13 +0200

    update audio to nixos defaults

--- modules/services/nixos/audio.nix
+++ modules/services/nixos/audio.nix
@@ -8,37 +8,5 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    extraConfig.pipewire = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 128;
-        "default.clock.min-quantum" = 64;
-        "default.clock.max-quantum" = 256;
-      };
-    };
-
-    extraConfig.pipewire-pulse = {
-      "context.modules" = [
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            "pulse.min.req" = "64/48000";
-            "pulse.default.req" = "128/48000";
-            "pulse.max.req" = "256/48000";
-            "pulse.min.quantum" = "64/48000";
-            "pulse.max.quantum" = "256/48000";
-          };
-        }
-      ];
-
-      "stream.properties" = {
-        "node.latency" = "128/48000";
-        "resample.quality" = 5;
-        "session.suspend-timeout-seconds" = 5;
-        "node.autoconnect" = true;
-      };
-    };
   };
-
 }
