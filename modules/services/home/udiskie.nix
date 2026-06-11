462761b88febc73a3ea1908aab688e036bf9658c
blob5 <101657201+blob5@users.noreply.github.com>
2026-06-11 23:20:25 +0200

    update udiskie service

--- modules/services/home/udiskie.nix
+++ modules/services/home/udiskie.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, hostSettings, ... }:
+let
+  role = hostSettings.role or "desktop";
+  isGraphical = role != "server";
+in
 {
   services.udiskie = {
-    enable = true;
+    enable = isGraphical;
     settings = {
       program_options = {
         file_manager = "${pkgs.nautilus}/bin/nautilus";
