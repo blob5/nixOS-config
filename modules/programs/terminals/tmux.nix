c808cb95c7307eac942cd6db26a82c544044b78e
blob5 <101657201+blob5@users.noreply.github.com>
2026-07-01 16:20:11 +0200

    update tmux config

--- modules/programs/terminals/tmux.nix
+++ modules/programs/terminals/tmux.nix
@@ -2,7 +2,16 @@
 
 {
   programs.tmux = {
+    mouse = true;
+    focusEvents = true;
+    aggressiveResize = true;
+    clock24 = true;
+    historyLimit = 50000;
     enable = true;
+    escapeTime = 0;
+    terminal = "screen-256color";
+
+    
     plugins = [
       pkgs.tmuxPlugins.sensible
       pkgs.tmuxPlugins.tokyo-night-tmux
